3c4879053cf1bc5ac0022f486b54f91d45369653
John-hackx <myofficialsat@gmail.com>
2025-04-03 08:04:24 +0000

    implemented contact and project desktop view

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,5 +1,108 @@
-function ContactPage() {
-  return <div>Contact</div>;
+import Footer from "../Components/Footer";
+import Header from "../Components/Header";
+import styles from "./ContactPage.module.css";
+import facebookLogo from "../assets/images/facebook.png";
+import twitterLogo from "../assets/images/twitter.png";
+import youtubeLogo from "../assets/images/youtube.png";
+import linkedinLogo from "../assets/images/linkedin.png";
+
+function ContactPage({ windowWidth }) {
+  return (
+    <div className={styles.contactPage}>
+      <Header windowWidth={windowWidth} />
+      <div className={styles.contactContainer}>
+        <div className={styles.left}>
+          <div className={styles.getInTouchContainer}>
+            <div className={styles.getInTouchHeader}>
+              <p>Get In Touch</p>
+              <span></span>
+            </div>
+            <div className={styles.getInTouchText}>
+              <p>
+                Have questions or need assistance? We're here to help! Reach out
+                to us for inquiries, partnerships.
+              </p>
+            </div>
+            <div className={styles.getInTouchContact}>
+              <div className={styles.phone}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="currentColor"
+                >
+                  <path d="M798-120q-125 0-247-54.5T329-329Q229-429 174.5-551T120-798q0-18 12-30t30-12h162q14 0 25 9.5t13 22.5l26 140q2 16-1 27t-11 19l-97 98q20 37 47.5 71.5T387-386q31 31 65 57.5t72 48.5l94-94q9-9 23.5-13.5T670-390l138 28q14 4 23 14.5t9 23.5v162q0 18-12 30t-30 12Z" />
+                </svg>
+                <div className={styles.phoneDetails}>
+                  <p className={styles.phoneDetailsTitle}>Phone</p>
+                  <p>(233) 24 851 9174</p>
+                </div>
+              </div>
+              <div className={styles.email}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="currentColor"
+                >
+                  <path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h640q33 0 56.5 23.5T880-720v480q0 33-23.5 56.5T800-160H160Zm320-280 320-200v-80L480-520 160-720v80l320 200Z" />
+                </svg>
+                <div className={styles.emailDetails}>
+                  <p className={styles.emailDetailsTitle}>Email</p>
+                  <p>easternfarms16@gmail.com</p>
+                </div>
+              </div>
+              <div className={styles.location}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="24px"
+                  viewBox="0 -960 960 960"
+                  width="24px"
+                  fill="currentColor"
+                >
+                  <path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 400Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Z" />
+                </svg>
+                <div className={styles.locationDetails}>
+                  <p className={styles.locationDetailsTitle}>Address</p>
+                  <p>15th Street Comm. 18 Tema - Ghana</p>
+                </div>
+              </div>
+            </div>
+            <div className={styles.getInTouchSocial}>
+              <img src={facebookLogo} alt="facebook-logo" />
+              <img src={youtubeLogo} alt="youtube-logo" />
+              <img src={twitterLogo} alt="twitter-logo" />
+              <img src={linkedinLogo} alt="linkedin-logo" />
+            </div>
+          </div>
+        </div>
+        <div className={styles.right}>
+          <div className={styles.contactFormContainer}>
+            <div className={styles.formTitle}>
+              <p>Contact Us</p>
+            </div>
+            <div className={styles.form}>
+              <form>
+                <input
+                  type="text"
+                  placeholder="Your name"
+                  name="name"
+                  required
+                />
+                <input type="text" placeholder="Email" name="email" required />
+                <input type="phone" placeholder="Phone" name="phone" required />
+                <textarea name="text-area" placeholder="Type your enquiry..." />
+                <button type="submit">Send</button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
 }
 
 export default ContactPage;
--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -1,5 +1,90 @@
-function ProjectsPage() {
-  return <div>Projects</div>;
+import Header from "../Components/Header";
+import styles from "./ProjectsPage.module.css";
+import facebookLogo from "../assets/images/facebook.png";
+import twitterLogo from "../assets/images/twitter.png";
+import youtubeLogo from "../assets/images/youtube.png";
+import linkedinLogo from "../assets/images/linkedin.png";
+import eggsImage from "../assets/images/eggs.jpg";
+import Footer from "../Components/Footer";
+
+function ProjectsPage({ windowWidth }) {
+  return (
+    <div className={styles.projectsPage}>
+      <Header windowWidth={windowWidth} />
+      <div className={styles.projectsContainer}>
+        <div className={styles.projectsHeader}>
+          <div className={styles.headerLeft}>
+            <p>Our Projects</p>
+            <h3>Creating Impacts in Global Food Market</h3>
+          </div>
+          <div className={styles.headerRight}>
+            <img src={facebookLogo} alt="facebook-logo" />
+            <img src={youtubeLogo} alt="youtube-logo" />
+            <img src={twitterLogo} alt="twitter-logo" />
+            <img src={linkedinLogo} alt="linkedin-logo" />
+          </div>
+        </div>
+        <div className={styles.projectsContent}>
+          <div className={styles.projectsContentContainer}>
+            <div className={styles.projectsContentSubContainer}>
+              <ProjectCard />
+              <ProjectCard />
+              <ProjectCard />
+              <ProjectCard />
+              <ProjectCard />
+              <ProjectCard />
+            </div>
+          </div>
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+}
+
+function ProjectCard() {
+  return (
+    <div className={styles.projectCard}>
+      <div className={styles.projectImage}>
+        <img src={eggsImage} alt="eggs-image" />
+      </div>
+      <div className={styles.projectDescription}>
+        <div className={styles.title}>
+          <h3>Large-scale Cocoa Plantation</h3>
+        </div>
+        <div className={styles.description}>
+          <p>
+            Developed a 500-hectare cocoa plantation, producing high-quality
+            cocoa beans for export to international market.
+          </p>
+        </div>
+        <div className={styles.location}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="currentColor"
+          >
+            <path d="M480-480q33 0 56.5-23.5T560-560q0-33-23.5-56.5T480-640q-33 0-56.5 23.5T400-560q0 33 23.5 56.5T480-480Zm0 400Q319-217 239.5-334.5T160-552q0-150 96.5-239T480-880q127 0 223.5 89T800-552q0 100-79.5 217.5T480-80Z" />
+          </svg>
+          <p>Adeiso - Eastern Region</p>
+        </div>
+        <div className={styles.date}>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            height="24px"
+            viewBox="0 -960 960 960"
+            width="24px"
+            fill="currentColor"
+          >
+            <path d="M480-400q-17 0-28.5-11.5T440-440q0-17 11.5-28.5T480-480q17 0 28.5 11.5T520-440q0 17-11.5 28.5T480-400Zm-160 0q-17 0-28.5-11.5T280-440q0-17 11.5-28.5T320-480q17 0 28.5 11.5T360-440q0 17-11.5 28.5T320-400Zm320 0q-17 0-28.5-11.5T600-440q0-17 11.5-28.5T640-480q17 0 28.5 11.5T680-440q0 17-11.5 28.5T640-400ZM480-240q-17 0-28.5-11.5T440-280q0-17 11.5-28.5T480-320q17 0 28.5 11.5T520-280q0 17-11.5 28.5T480-240Zm-160 0q-17 0-28.5-11.5T280-280q0-17 11.5-28.5T320-320q17 0 28.5 11.5T360-280q0 17-11.5 28.5T320-240Zm320 0q-17 0-28.5-11.5T600-280q0-17 11.5-28.5T640-320q17 0 28.5 11.5T680-280q0 17-11.5 28.5T640-240ZM200-80q-33 0-56.5-23.5T120-160v-560q0-33 23.5-56.5T200-800h40v-80h80v80h320v-80h80v80h40q33 0 56.5 23.5T840-720v560q0 33-23.5 56.5T760-80H200Zm0-80h560v-400H200v400Z" />
+          </svg>
+          <p>Jan - March 2022</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default ProjectsPage;
